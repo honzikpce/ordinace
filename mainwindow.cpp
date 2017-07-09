0e05883ed6fca41ead40ebc858ee997df2780539
Jan Novak <jan.nov8k@gmail.com>
2017-07-09 21:21:59 +0200

    pripojeni databaze, napojeni na tableView a filtrovani.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <QSqlDatabase>
 #include <QtDebug>
 #include <QSqlError>
+#include <QSqlQuery>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -14,14 +16,54 @@ MainWindow::MainWindow(QWidget *parent) :
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("localhost");
     db.setDatabaseName("ordinace");
-    db.setUserName("root");
-    db.setPassword("");
+    db.setUserName("ordinace");
+    db.setPassword("ordinace");
     bool ok = db.open();
-    if (!ok) qDebug() << "Chyba : Nepodarilo se pripojit databazi! "  << db.lastError().text();
+    if (ok) qDebug() << "Pripojeni k databazi - OK.";
+    else qDebug() << "Chyba : Nepodarilo se pripojit databazi! "  << db.lastError().text();
 
+    // create model for pacients
+    modelPacients = new QSqlQueryModel(this);
+    baseQuerry = "SELECT rc, CONCAT(UPPER(prijmeni), ' ', jmeno, ' ', titulPred, ' ', titulZa) AS fullName FROM pacient";
+    modelPacients->setQuery(baseQuerry);
+
+    //bind widgets to model
+    ui->tableViewPacients->setModel(modelPacients);
+
+    // adjust table view appearance
+    ui->tableViewPacients->setAlternatingRowColors(true);
+    QFont newFont("Tahoma", 10, QFont::Normal, false);
+    ui->tableViewPacients->setFont(newFont);
+    ui->tableViewPacients->horizontalHeader()->setStretchLastSection(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete modelPacients;
+}
+
+void MainWindow::on_rcLineEdit_textChanged(const QString &arg1)
+{
+    QSqlQuery query;
+    query.prepare(baseQuerry + " WHERE rc LIKE :rc AND prijmeni LIKE :surname");
+    query.bindValue(":rc", ui->rcLineEdit->text() + "%");
+    query.bindValue(":surname", ui->surnameLineEdit->text() + "%");
+
+
+    query.exec();
+    modelPacients->setQuery(query);
+
+}
+
+void MainWindow::on_surnameLineEdit_textChanged(const QString &arg1)
+{
+    QSqlQuery query;
+    query.prepare(baseQuerry + " WHERE rc LIKE :rc AND prijmeni LIKE :surname");
+    query.bindValue(":rc", ui->rcLineEdit->text() + "%");
+    query.bindValue(":surname", ui->surnameLineEdit->text() + "%");
+
+
+    query.exec();
+    modelPacients->setQuery(query);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlRelationalTableModel>
+#include <QSqlQueryModel>
+#include <QtCore>
+#include <QtGui>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +19,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_rcLineEdit_textChanged(const QString &arg1);
+
+    void on_surnameLineEdit_textChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
+    QSqlQueryModel *modelPacients = nullptr;
+    QString baseQuerry;
 };
 
 #endif // MAINWINDOW_H
